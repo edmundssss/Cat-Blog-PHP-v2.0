bfd305c270594936b887aad4cbee7e03842cdfbc
Edmunds Putnis <33813208+edmundssss@users.noreply.github.com>
2018-04-01 23:07:08 +0300

    Add files via upload
    Cat blog PHP Riga Coding School

---
+++ rcs-web-course1/components/footer.php
@@ -0,0 +1,24 @@
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                   <b>&copy; Edo, 2018, All rights reserved.<b>
+                </div>
+            </div>    
+        </div>
+    </footer>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+    <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+</body>
+</html>
---
+++ rcs-web-course1/components/index.php
@@ -0,0 +1,102 @@
+<main>
+    <div class="container">
+    <div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
+    <!-- Carousel indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>   
+    <!-- Carousel items -->
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="https://images.unsplash.com/photo-1511876849877-f8d0d953d2e0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a005647cf42ef9a8c66e1aed85ccc5ec&auto=format&fit=crop&w=1050&q=80" alt="First Slide">
+            <div class="carousel-caption">
+                <h3>Cat1</h3>
+                <p>Lorem ipsum dolor sit amet...</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="https://images.unsplash.com/photo-1512873897628-eea05c840147?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e15fc03eb441dad7770ea11572272885&auto=format&fit=crop&w=1050&q=80" alt="Second Slide">
+            <div class="carousel-caption">
+                <h3>Cat2</h3>
+                <p>Aliquam sit amet gravida nibh, facilisis...</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="https://images.unsplash.com/photo-1460572894071-bde5697f7197?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0680b44a0d2a26c1147bada4d1727719&auto=format&fit=crop&w=1053&q=80" alt="Third Slide">
+            <div class="carousel-caption">
+                <h3>Cat3</h3>
+                <p>Praesent commodo cursus magna vel...</p>
+            </div>
+        </div>
+    </div>
+    <!-- Carousel nav -->
+    <a class="carousel-control left" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a class="carousel-control right" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+    </a>
+</div>
+            <div class="row articles-row">
+                <div class="col-xs-12 col-sm-6 col-md-4 small-article">
+                    <div class="row">
+                        <div class="col-sm-4 hidden-xs">
+                            <a href="index.php?page=article">
+                                <img class="thumbnail" src="https://images.unsplash.com/photo-1511876849877-f8d0d953d2e0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a005647cf42ef9a8c66e1aed85ccc5ec&auto=format&fit=crop&w=1050&q=80" alt="">
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-8">
+                            <h4>
+                                <a href="index.php?page=article">
+                                    Title 1
+                                </a>
+                            </h4>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nostrum cum officia, ut quod veniam tempore? Nesciunt optio ducimus nemo perspiciatis, aspernatur maxime, laudantium quo atque non, saepe illo ratione?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4 small-article">
+                    <div class="row">
+                        <div class="col-sm-4 hidden-xs">
+                            <a href="index.php?page=article">
+                                <img class="thumbnail" src="https://images.unsplash.com/photo-1512873897628-eea05c840147?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e15fc03eb441dad7770ea11572272885&auto=format&fit=crop&w=1050&q=80" alt="">
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-8">
+                            <h4>
+                                <a href="index.php?page=article">
+                                    Title 2
+                                </a>
+                            </h4>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nostrum cum officia, ut quod veniam tempore? Nesciunt optio ducimus nemo perspiciatis, aspernatur maxime, laudantium quo atque non, saepe illo ratione?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4 small-article">
+                    <div class="row">
+                        <div class="col-sm-4 hidden-xs">
+                            <a href="index.php?page=article">
+                                <img class="thumbnail" src="https://images.unsplash.com/photo-1460572894071-bde5697f7197?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0680b44a0d2a26c1147bada4d1727719&auto=format&fit=crop&w=1053&q=80" alt="">
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-8">
+                            <h4>
+                                <a href="index.php?page=article">
+                                    Title 3
+                                </a>
+                            </h4>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nostrum cum officia, ut quod veniam tempore? Nesciunt optio ducimus nemo perspiciatis, aspernatur maxime, laudantium quo atque non, saepe illo ratione?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</main>
